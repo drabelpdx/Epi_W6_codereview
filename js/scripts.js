5e5bc478cddc3a1b749c575133f313e3548e726d
DavidAbel <drabelpdx@gmail.com>
2015-05-29 09:17:01 -0700

    Adds initial files.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var findReplace = function(sentence, word1, word2) {
+
+};
+
+  // $(document).ready(function() {
+  //   $("form#wordcount").submit(function(event) {
+  //     var sentence = $("input#sentence").val();
+  //     var sentence = $("input#word1").val();
+  //     var sentence = $("input#word2").val();
+  //     var result = findReplace(sentence, word1, word2);
+  //     console.log(sentence)
+  //     console.log(word1)
+  //     console.log(word2)
+  //     console.log(result)
+  //     $(".words").html(result);
+  //
+  //     $("#result").show();
+  //
+  //     event.preventDefault();
+  //   });
+  // });
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe('findReplace', function() {
+
+  it("(find and return a word from a string) {
+    expect(findReplace("This is my sentence")).to.equal("sentence");
+  });
+});
